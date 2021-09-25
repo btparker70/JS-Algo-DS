f3341b5424452d225625a10935cdf3a70c1b88bc
Brian Parker <btparker70@gmail.com>
2021-09-25 16:45:05 -0700

    added same frequency problem

---
+++ 5. Problem_Solving_Patterns/OptionalChallenges.js
@@ -0,0 +1,42 @@
+// check if 2 numbers have the same digits any order
+
+function sameFrequency(num1, num2){
+    let strNum1 = num1.toString();
+    let strNum2 = num2.toString();
+    if(strNum1.length !== strNum2.length) return false;
+    
+    let countNum1 = {};
+    let countNum2 = {};
+    
+    for(let i = 0; i < strNum1.length; i++){
+      countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+    }
+    
+    for(let j = 0; j < strNum1.length; j++){
+      countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+    }
+    
+    for(let key in countNum1){
+      if(countNum1[key] !== countNum2[key]) return false;
+    }
+   
+    return true;
+  }
+
+
+// check if list of arguments has duplicates
+// ex (1, 2, 3, 3)
+
+function areThereDuplicates() {
+  // good luck. (supply any arguments you deem necessary.)
+  let collection = {};
+  for (let val in arguments) {
+      collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+  }
+  for (let key in collection) {
+      if(collection[key] !== 1) {
+          return true
+      }
+  }
+  return false
+}
