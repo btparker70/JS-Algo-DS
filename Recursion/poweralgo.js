b013127b4733e746b3210e3267d9265270f2ebcc
Brian Parker <btparker70@gmail.com>
2021-10-27 23:02:56 -0700

    problem 1

---
+++ Recursion/poweralgo.js
@@ -0,0 +1,12 @@
+// this function accepts a base and exponent. 
+// function should return the power of the bsae to the xponent. 
+// this function should mimint he functionality of Math.pow()
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exponent){
+  if(exponent === 0) return 1;
+return base *= power(base, (exponent - 1));
+}
