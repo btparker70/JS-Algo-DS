6616caf7ea480eb112cb814c3f98d365b418c151
Brian Parker <btparker70@gmail.com>
2021-10-27 23:42:47 -0700

    prob1 comments

--- Recursion/poweralgo.js
+++ Recursion/poweralgo.js
@@ -10,3 +10,11 @@ function power(base, exponent){
   if(exponent === 0) return 1;
 return base *= power(base, (exponent - 1));
 }
+power(2, 4)
+
+// 2 *= power(2, 3) => 2 *= power(2, 2) => 2 *= power(2, 1) => 2 *= 1
+// 2 *= power(2, 3) => 2 *= power(2, 2) => 2 *= power(2, 1) => 2
+// 2 *= power(2, 3) => 2 *= power(2, 2) => 2 *= 2
+// 2 *= power(2, 3) => 2 *= 2 *= 2
+// 2 *= 2 *= 2 *= 2
+// same as 2^4
