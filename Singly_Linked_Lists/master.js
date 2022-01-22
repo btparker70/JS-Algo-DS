93febfb27c6fc9757468be6620f8899a4d5f9f8e
Brian Parker <btparker70@gmail.com>
2022-01-22 00:02:03 -0800

    added set

--- Singly_Linked_Lists/master.js
+++ Singly_Linked_Lists/master.js
@@ -79,6 +79,14 @@ class SinglyLinkedList {
     };
     return current;
   }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 // var first = new Node('Hi')
