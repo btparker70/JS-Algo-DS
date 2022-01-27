40dce02976c90c53f26fe2be19dd1e76052fa942
Brian Parker <btparker70@gmail.com>
2022-01-26 22:30:08 -0800

    bst insert!

--- Trees/Binary_Search_Trees.js
+++ Trees/Binary_Search_Trees.js
@@ -10,10 +10,36 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode; 
+      return this;
+    }
+    let current = this.root;
+    while(true) {
+      if(val === current.val) return undefined
+      if(val < current.val) {
+        if(current.left === null) {
+          current.left = newNode;
+          return this;
+        } current = current.left;
+      } else {
+        if(current.right === null) {
+          current.right = newNode;
+          return this;
+        } current = current.right;
+      }
+    }
+    
+  }
 }
 
-let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9)
+var tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
